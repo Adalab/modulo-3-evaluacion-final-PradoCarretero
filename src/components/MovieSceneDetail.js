8e0e150030981e6124b1015870f27d788217ed8c
PradoCarretero <p.carreteroayala@gmail.com>
2022-05-05 19:24:24 +0200

    styles Movie Scene Detail

--- src/components/MovieSceneDetail.js
+++ src/components/MovieSceneDetail.js
@@ -1,14 +1,23 @@
+import "../styles/MovieSceneDetail.scss";
+
 function MovieSceneDetail(props) {
   return (
-    <section key={props.movie.index}>
-      <img className="movie__poster" src={props.movie.poster} alt="" />
-      <p>Nombre: {props.movie.movie}</p>
-      <p>Año: {props.movie.year}</p>
-      <p>Frase: {props.movie.quote}</p>
-      <p>Director: {props.movie.director}</p>
-      <a href={props.movie.audio} target="_blank" rel="noreferrer">
-        Escucha la frase
-      </a>
+    <section key={props.movie.index} className="detail__wrapper">
+      <img className="detail__poster" src={props.movie.poster} alt="" />
+      <article className="detail__text">
+        <p>Nombre: {props.movie.movie}</p>
+        <p>Año: {props.movie.year}</p>
+        <p>Director: {props.movie.director}</p>
+        <p className="detail__quote">Frase: {props.movie.quote}</p>
+        <a
+          className="detail__button"
+          href={props.movie.audio}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Escucha la frase
+        </a>
+      </article>
     </section>
   );
 }
