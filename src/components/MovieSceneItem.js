f251d4b007e9a04a7791b0ad30896cd8db8a2721
PradoCarretero <p.carreteroayala@gmail.com>
2022-05-05 18:58:36 +0200

    finish styles

--- src/components/MovieSceneItem.js
+++ src/components/MovieSceneItem.js
@@ -5,13 +5,15 @@ function MovieSceneItem(props) {
   return (
     <Link className="movie__link" to={`/movie/${props.movie.id}`}>
       <img className="movie__poster" src={props.movie.poster} alt="" />
-
-      <article className="movie__text">
-        <p>
-          {props.movie.movie} - {props.movie.year}
-        </p>
-        <p className="movie__quote">{props.movie.quote}</p>
-      </article>
+      <div className="movie__info">
+        <article>
+          <p>
+            {props.movie.movie} - {props.movie.year}
+          </p>
+          <p className="movie__quote">{props.movie.quote}</p>
+        </article>
+        <i className="fa-solid fa-circle-plus fa-xl movie__icon"></i>
+      </div>
     </Link>
   );
 }
